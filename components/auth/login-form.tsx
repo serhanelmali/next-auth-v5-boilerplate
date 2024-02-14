7ecce81577d30fcf323bb1cfdbc79b713ef57351
Serhan Elmali <serhanelmali@gmail.com>
2024-02-15 01:04:18 +0300

    feat: implement form validation and submission for login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,28 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "@/components/ui/button";
+import { LoginSchema } from "@/schemas";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => console.log(values);
+
   return (
     <CardWrapper
       headerLabel="Welcome back again!"
@@ -8,7 +30,42 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       displaySocial
     >
-      <div>Login Form</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((values) => onSubmit(values))}
+          className="space-y-5"
+        >
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
