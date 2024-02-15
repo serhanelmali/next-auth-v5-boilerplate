0a2f3e3f5583850151e36b5c889bb722ddbcd7a4
Serhan Elmali <serhanelmali@gmail.com>
2024-02-15 16:16:45 +0300

    refactor: Remove unnecessary comment and optimize user registration logic

--- actions/register.tsx
+++ actions/register.tsx
@@ -1,14 +1,36 @@
 "use server";
 
-import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import { RegisterSchema } from "@/schemas";
+import bcryptjs from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (validatedFields.success) {
+    const { email, name, password } = validatedFields.data;
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const isUserExists = await getUserByEmail(email);
+
+    if (isUserExists) {
+      return { error: "User already exists." };
+    }
+
+    await db.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    // TODO: Send email verification token
+
     return {
-      success: "Success.",
+      success: "Verification email sent.",
     };
   } else {
     return {
